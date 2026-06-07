d4dd42a7eb339dbd1d76d21cc69f778a83189d78
Husno130 <husno130@gmail.com>
2026-06-07 22:30:48 +0500

    .

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,3 +1,3 @@
-const API = import.meta.env.VITE_API || "https://test-deploy-0xnp.onrender.com" //"http://127.0.0.1:8000";
+const API = import.meta.env.VITE_API || "http://127.0.0.1:8000";
 
 export default API; 
